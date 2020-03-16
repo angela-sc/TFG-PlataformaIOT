623912dbed762769640e9bc72d22a1a59c312b75
Ángela <asc134@gcloud.ua.es>
2020-03-16 11:30:30 +0100

    [Added] EntidadUsuario y EntidadProyecto

---
+++ Libreria/Entidades/EntidadUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libreria.Entidades
+{
+    class EntidadUsuario
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public Byte[] password { get; set; }
+        public int FK_projectID { get; set; }
+    }
+}
