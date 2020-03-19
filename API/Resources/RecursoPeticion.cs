8af7999db64478d26a6e64a9164a282be70fc14f
Ángela <asc134@gcloud.ua.es>
2020-03-19 20:31:03 +0100

    [Added] Eliminada plantilla API WEB + creada API vacía

---
+++ API/Resources/RecursoPeticion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using CoAP.Server.Resources;
+
+namespace API.Resources
+{
+    public class RecursoPeticion : Resource
+    {
+        public RecursoPeticion() : base("peticion")
+        {
+
+        }
+    }
+}
