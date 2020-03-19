b00d76e0ce2a50ad8ab459b3343fd22ad29561d8
Alejandro Box <abox@solidq.com>
2020-03-19 17:16:21 +0100

    [Added] Prueba de API tradicional

---
+++ Pruebas.ClienteConsola/ClientHelper.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas.ClienteConsola
+{
+    public class ClientHelper
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string ApiUri = "http://localhost:65243";
+
+        public static async Task<string> GetAsync()
+        {
+            var responseString = await client.GetStringAsync($"{ApiUri}/api/WeatherForecast");
+            return responseString;
+        }
+
+        public static async Task<string> PostAsync()
+        {
+            var values = new Dictionary<string, string>
+            {
+            { "thing1", "hello" },
+            { "thing2", "world" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync($"{ApiUri}/recepticle.aspx", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+        }
+    }
+}
---
+++ Pruebas.ClienteConsola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Pruebas.ClienteConsola
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //await TestGet_ApiNormal();
+            await TestGet_ApiCoap();
+        }
+
+        private static async Task TestGet_ApiNormal()
+        {
+            var respuesta = await ClientHelper.GetAsync();
+            Console.WriteLine(respuesta);
+        }
+
+        private static async Task TestGet_ApiCoap()
+        {
+
+        }
+    }
+}
