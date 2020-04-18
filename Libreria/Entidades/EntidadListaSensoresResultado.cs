6424968fde4dab5c78804c43a9ff9b15f4220d52
Ángela <asc134@gcloud.ua.es>
2020-04-18 17:01:39 +0200

    [Added] clase resultado (EntidadListaSensoresResultado): datos del sensor en la vista de una estacion bae

---
+++ Libreria/Entidades/EntidadListaSensoresResultado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libreria.Entidades
+{
+    public class EntidadListaSensoresResultado
+    {
+        public string nombreSensor;
+        public float ultimaTemperatura;
+        public DateTime fechaTemperatura;
+        public float ultimaHumedad;
+        public DateTime fechaHumedad;
+
+    }
+}
