3e7fc9303ad92ebf74b99afedb178a26d44c2145
Ángela <asc134@gcloud.ua.es>
2020-04-28 12:52:52 +0200

    [Añadido] PaginaRegistroUsuario.razor + RegistroUsuarioViewModel

---
+++ PortalWeb/ViewModel/RegistroUsuarioViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalWeb.ViewModel
+{
+    public class RegistroUsuarioViewModel : ComponentBase
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Nombre demasiado largo.")]
+        public string name { get; set; }
+
+    }
+}
