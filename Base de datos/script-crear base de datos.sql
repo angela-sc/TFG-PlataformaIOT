414be54afa308a7fe257e0cdf5d687768b21b952
Alejandro Box <abox@solidq.com>
2020-05-06 19:52:49 +0200

    [Updated] Script para crear BD mejorado, calves primarias compuestas descartadas

--- Base de datos/script-crear base de datos.sql
+++ Base de datos/script-crear base de datos.sql
@@ -11,55 +11,52 @@ DROP TABLE IF EXISTS [plataforma_iot]..[Usuario];
 
 --CREAR TABLAS DE LA BASE DE DATOS
 CREATE TABLE [plataforma_iot]..[Usuario](
-	[id] INT IDENTITY(1,1) PRIMARY KEY,
-	[email] VARCHAR(48) NOT NULL,
-	[contrasenya] VARBINARY(128) NOT NULL,
-	[nombre] VARCHAR(30) NOT NULL,
-	[apellidos] VARCHAR(30) NOT NULL
+	[id]			INT IDENTITY(1,1)	PRIMARY KEY,
+	[email]			VARCHAR(48)			UNIQUE	NOT NULL,
+	[contrasenya]	VARBINARY(128)				NOT NULL,
+	[nombre]		VARCHAR(30)					NOT NULL,
+	[apellidos]		VARCHAR(30)					NOT NULL
 );
 
 CREATE TABLE [plataforma_iot]..[Proyecto](
-	[id] INT IDENTITY(1,1) PRIMARY KEY,
-	[nombre] VARCHAR(128) NOT NULL,
-	[descripcion] NVARCHAR(MAX)
+	[id]			INT	IDENTITY(1,1)	PRIMARY KEY,
+	[nombre]		VARCHAR(128)		UNIQUE		NOT NULL,
+	[descripcion]	NVARCHAR(MAX)
 );
 
 CREATE TABLE [plataforma_iot]..[Usuario_en_Proyecto](
-	[id_usuario] INT ,
-	[id_proyecto] INT,
-	CONSTRAINT [pk_usuarioenproyecto] PRIMARY KEY ([id_usuario],[id_proyecto]),
-	CONSTRAINT [fk_usuarioenproyecto_usuario] FOREIGN KEY ([id_usuario]) REFERENCES [plataforma_iot]..[Usuario]([id]) ON DELETE CASCADE,
-	CONSTRAINT [fk_usuarioenproyecto_proyecto] FOREIGN KEY ([id_proyecto]) REFERENCES [plataforma_iot]..[Proyecto]([id]) ON DELETE CASCADE
+	[id_usuario]	INT,
+	[id_proyecto]	INT,
+	CONSTRAINT [pk_usuarioenproyecto]			PRIMARY KEY ([id_usuario],[id_proyecto]),
+	CONSTRAINT [fk_usuarioenproyecto_usuario]	FOREIGN KEY ([id_usuario])	REFERENCES [plataforma_iot]..[Usuario]([id])	ON DELETE CASCADE,
+	CONSTRAINT [fk_usuarioenproyecto_proyecto]	FOREIGN KEY ([id_proyecto]) REFERENCES [plataforma_iot]..[Proyecto]([id])	ON DELETE CASCADE
 );
 
 CREATE TABLE [plataforma_iot]..[EstacionBase](
-	[id] INT IDENTITY(1,1),
-	[nombre] VARCHAR(10) NOT NULL,
-	[fk_idproyecto] INT,
-	CONSTRAINT [pk_estacionbase] PRIMARY KEY ([id],[fk_idproyecto]),
-	CONSTRAINT [fk_estacionbase_proyecto] FOREIGN KEY ([fk_idproyecto]) REFERENCES [plataforma_iot]..[Proyecto]([id]) ON DELETE CASCADE
+	[id]				INT IDENTITY(1,1)	PRIMARY KEY,
+	[nombre]			VARCHAR(10)			NOT NULL,
+	[fk_idproyecto]		INT					NOT NULL,
+	CONSTRAINT [uk_estacionbase]			UNIQUE ([nombre],[fk_idproyecto]),
+	CONSTRAINT [fk_estacionbase_proyecto]	FOREIGN KEY ([fk_idproyecto]) REFERENCES [plataforma_iot]..[Proyecto]([id]) ON DELETE CASCADE
 );
 
 CREATE TABLE [plataforma_iot]..[Sensor](
-	[id] INT IDENTITY(1,1),
-	[nombre] CHAR(11) NOT NULL,
-	[longitud] VARCHAR(15) NOT NULL,
-	[latitud] VARCHAR(15) NOT NULL,
-	[fk_idestacionbase] INT,
-	[fk_idproyecto] INT NOT NULL,
-	CONSTRAINT [pk_sensor] PRIMARY KEY ([id],[fk_idestacionbase], [fk_idproyecto]),
-	CONSTRAINT [fk_sensor_estacionbase] FOREIGN KEY ([fk_idestacionbase],[fk_idproyecto]) REFERENCES [plataforma_iot]..[EstacionBase]([id],[fk_idproyecto]) ON DELETE CASCADE
+	[id]				INT IDENTITY(1,1)	PRIMARY KEY,
+	[nombre]			CHAR(11)			NOT NULL,
+	[longitud]			VARCHAR(15)			NOT NULL,
+	[latitud]			VARCHAR(15)			NOT NULL,
+	[fk_idestacionbase] INT					NOT NULL,
+	CONSTRAINT [uk_sensor]				UNIQUE ([nombre],[fk_idestacionbase]),
+	CONSTRAINT [fk_sensor_estacionbase] FOREIGN KEY ([fk_idestacionbase]) REFERENCES [plataforma_iot]..[EstacionBase]([id]) ON DELETE CASCADE
 );
 
 CREATE TABLE [plataforma_iot]..[Datos](
-	[stamp] DATETIME,
-	[fk_idsensor] INT,
-	[fk_idestacionbase] INT,
-	[fk_idproyecto] INT,
-	[humedad] FLOAT,
-	[temperatura] FLOAT,
-	CONSTRAINT [pk_datos] PRIMARY KEY ([stamp],[fk_idsensor], [fk_idestacionbase],[fk_idproyecto]),
-	CONSTRAINT [fk_datos_sensor] FOREIGN KEY ([fk_idsensor],[fk_idestacionbase],[fk_idproyecto]) REFERENCES [plataforma_iot]..[Sensor]([id],[fk_idestacionbase],[fk_idproyecto]) ON DELETE CASCADE
+	[stamp]			DATETIME,
+	[fk_idsensor]	INT,
+	[humedad]		FLOAT,
+	[temperatura]	FLOAT,
+	CONSTRAINT [pk_datos]			PRIMARY KEY ([stamp],[fk_idsensor]),
+	CONSTRAINT [fk_datos_sensor]	FOREIGN KEY ([fk_idsensor]) REFERENCES [plataforma_iot]..[Sensor]([id]) ON DELETE CASCADE
 );
 
 USE plataforma_iot
@@ -117,38 +114,38 @@ SET IDENTITY_INSERT [plataforma_iot]..[EstacionBase] OFF;
 SET IDENTITY_INSERT [plataforma_iot]..[Sensor] ON;
 
 INSERT INTO [plataforma_iot]..[Sensor]
-           ([id],[nombre],[longitud],[latitud],[fk_idestacionbase],[fk_idproyecto])
+           ([id],[nombre],[longitud],[latitud],[fk_idestacionbase])
      VALUES
-           (0,'SE00','-0,519801','38,278209',0,0),
-		   (1,'SE01','-0,528031','38,2781293',1,0),
-		   (2,'SE02','-0,530436','38,280800',2,1),
-		   (3,'SE03','-0,535455','38,275500',3,2);
+           (0,'SE00','-0,519801','38,278209',0),
+		   (1,'SE01','-0,528031','38,2781293',1),
+		   (2,'SE02','-0,530436','38,280800',2),
+		   (3,'SE03','-0,535455','38,275500',3);
 
 SET IDENTITY_INSERT [plataforma_iot]..[Sensor] OFF;
 
 ---- datos
 
 INSERT INTO [plataforma_iot]..[Datos]
-           ([stamp],[fk_idsensor],[fk_idestacionbase],[fk_idproyecto],[humedad],[temperatura])
+           ([stamp],[fk_idsensor],[humedad],[temperatura])
      VALUES
-           ('2020-03-31 11:55:00',0,0,0,40.75,20.05),
-		   ('2020-03-31 12:00:00',0,0,0,40.75,20.05),
-		   ('2020-03-31 12:05:00',0,0,0,40.75,20.05),
-		   ('2020-03-31 12:10:00',0,0,0,40.75,20.05),
+           ('2020-03-31 11:55:00',0,40.75,20.05),
+		   ('2020-03-31 12:00:00',0,40.75,20.05),
+		   ('2020-03-31 12:05:00',0,40.75,20.05),
+		   ('2020-03-31 12:10:00',0,40.75,20.05),
 
-		   ('2020-03-31 11:55:00',1,1,0,35.7,25.53),
-		   ('2020-03-31 12:00:00',1,1,0,35.8,25.55),
-		   ('2020-03-31 12:05:00',1,1,0,35.95,25.53),
+		   ('2020-03-31 11:55:00',1,35.7,25.53),
+		   ('2020-03-31 12:00:00',1,35.8,25.55),
+		   ('2020-03-31 12:05:00',1,35.95,25.53),
 
-		   ('2020-03-31 11:55:00',2,2,1,35.7,25.53),
-		   ('2020-03-31 12:00:00',2,2,1,35.8,25.55),
-		   ('2020-03-31 12:05:00',2,2,1,35.95,25.54),
-		   ('2020-03-31 12:10:00',2,2,1,35.89,25.55),
-		   ('2020-03-31 12:15:00',2,2,1,35.75,25.56),
-		   ('2020-03-31 12:20:00',2,2,1,35.8,25.53),
+		   ('2020-03-31 11:55:00',2,35.7,25.53),
+		   ('2020-03-31 12:00:00',2,35.8,25.55),
+		   ('2020-03-31 12:05:00',2,35.95,25.54),
+		   ('2020-03-31 12:10:00',2,35.89,25.55),
+		   ('2020-03-31 12:15:00',2,35.75,25.56),
+		   ('2020-03-31 12:20:00',2,35.8,25.53),
 
-		   ('2020-03-31 11:55:00',3,3,2,35.7,25.53),
-		   ('2020-03-31 12:00:00',3,3,2,35.8,25.55),
-		   ('2020-03-31 12:05:00',3,3,2,35.95,25.54);
+		   ('2020-03-31 11:55:00',3,35.7,25.53),
+		   ('2020-03-31 12:00:00',3,35.8,25.55),
+		   ('2020-03-31 12:05:00',3,35.95,25.54);
 
 EXEC sp_MSforeachtable 'SELECT * FROM ?';
