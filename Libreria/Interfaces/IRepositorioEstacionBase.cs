c280a839af6668b736c21c1f65e6ab4c1bb21db3
Ángela <asc134@gcloud.ua.es>
2020-05-06 18:54:19 +0200

    [modificado] cambiado metodo getid de repositorio estacion base por obtenerid

--- Libreria/Interfaces/IRepositorioEstacionBase.cs
+++ Libreria/Interfaces/IRepositorioEstacionBase.cs
@@ -9,7 +9,8 @@ namespace Libreria.Interfaces
     public interface IRepositorioEstacionBase
     {
         void InsertaEstacion(EntidadEstacionBase entidadEstacion);
-        Task<int> GetId(string nombreEstacionBase);
+        //Task<int> GetId(string nombreEstacionBase);
+        Task<int> ObtenerId(string estacionBase);
         Task<IEnumerable<EntidadSensorResultado>> ObtenerSensores(string nombreEstacionBase);
         // Task<IEnumerable<EntidadCoordenada>> ObtenerCoordenadasSensores(string nombreEstacionBase);
 
