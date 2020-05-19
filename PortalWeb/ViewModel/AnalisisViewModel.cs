5dc15d7b694ed02b5ccb6ad71b36305ae0d5dfc7
Ángela <asc134@gcloud.ua.es>
2020-05-19 11:20:13 +0200

    [añadido] empezada vista análisis

---
+++ PortalWeb/ViewModel/AnalisisViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Libreria.Entidades;
+using Libreria.Interfaces;
+using Microsoft.AspNetCore.Components;
+using PortalWeb.Data;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalWeb.ViewModel
+{
+    public class AnalisisViewModel : ComponentBase
+    {
+        protected IEnumerable<EntidadProyecto> proyectos;
+        
+
+        private IServicioProyecto servicioProyecto;
+        private int idUsuario;
+
+        protected override async Task OnInitializedAsync()
+        {
+            idUsuario = InformacionUsuario.IdUsuario;
+            servicioProyecto = new ServicioProyecto("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=plataforma_iot;Integrated Security=true", null);
+
+            proyectos = new List<EntidadProyecto>();
+            proyectos = await servicioProyecto.ObtenerProyectos(idUsuario);
+
+            this.StateHasChanged();
+        }
+    }
+}
