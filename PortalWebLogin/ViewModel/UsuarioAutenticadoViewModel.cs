3965aaf0b4277a1b11870dd991d426d13afcd4f9
Ángela <asc134@gcloud.ua.es>
2020-06-18 21:30:33 +0200

    [añadido] clase UsuarioAutenticadoViewModel.cs > funcion OnParameterSetAsync (controla si el usuario se ha autenticado) > settea el id del usuario actual

---
+++ PortalWebLogin/ViewModel/UsuarioAutenticadoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PortalWebLogin.ViewModel
+{
+    public class UsuarioAutenticadoViewModel : ComponentBase
+    {
+        protected string idUsuario { get; private set; }
+        [CascadingParameter]
+        protected Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var usuario = (await authenticationStateTask).User;
+
+            if (!usuario.Identity.IsAuthenticated)
+            {
+                NavigationManager.NavigateTo("Identity/Account/Login");
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(idUsuario))
+                {
+                    idUsuario = usuario.FindFirst(ClaimTypes.NameIdentifier).Value;
+                }
+            }
+        }
+    }
+}
