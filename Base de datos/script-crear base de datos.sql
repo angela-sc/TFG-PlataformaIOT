f23c5e468c01f6967c17b253cd020f7b133070f1
Alejandro Box <abox@solidq.com>
2020-06-22 11:35:01 +0200

    [Añadido] Tablas para login en script de creacion de base de datos

--- Base de datos/script-crear base de datos.sql
+++ Base de datos/script-crear base de datos.sql
@@ -1,7 +1,18 @@
 USE plataforma_iot
-
 GO
+
+DECLARE @DatosDePrueba BIT = 1 --> 1 = Sí, 0 = No
+
 --ELIMINAR TABLAS DE LA BASE DE DATOS
+DROP TABLE IF EXISTS [dbo].[AspNetRoleClaims]
+DROP TABLE IF EXISTS [dbo].[AspNetUserClaims]
+DROP TABLE IF EXISTS [dbo].[AspNetUserLogins]
+DROP TABLE IF EXISTS [dbo].[AspNetUserRoles]
+DROP TABLE IF EXISTS [dbo].[AspNetUserTokens]
+DROP TABLE IF EXISTS [dbo].[AspNetRoles]
+DROP TABLE IF EXISTS [dbo].[AspNetUsers]
+DROP TABLE IF EXISTS [dbo].[__EFMigrationsHistory]
+
 DROP TABLE IF EXISTS [plataforma_iot]..[Datos];
 DROP TABLE IF EXISTS [plataforma_iot]..[Sensor];
 DROP TABLE IF EXISTS [plataforma_iot]..[EstacionBase];
@@ -10,6 +21,141 @@ DROP TABLE IF EXISTS [plataforma_iot]..[Proyecto];
 DROP TABLE IF EXISTS [plataforma_iot]..[Usuario];
 
 --CREAR TABLAS DE LA BASE DE DATOS
+CREATE TABLE [dbo].[__EFMigrationsHistory](
+	[MigrationId] [nvarchar](150) NOT NULL,
+	[ProductVersion] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY CLUSTERED 
+(
+	[MigrationId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[AspNetRoles](
+	[Id] [nvarchar](450) NOT NULL,
+	[Name] [nvarchar](256) NULL,
+	[NormalizedName] [nvarchar](256) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetRoles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+CREATE TABLE [dbo].[AspNetUsers](
+	[Id] [nvarchar](450) NOT NULL,
+	[UserName] [nvarchar](256) NULL,
+	[NormalizedUserName] [nvarchar](256) NULL,
+	[Email] [nvarchar](256) NULL,
+	[NormalizedEmail] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEnd] [datetimeoffset](7) NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+ CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+CREATE TABLE [dbo].[AspNetRoleClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+ALTER TABLE [dbo].[AspNetRoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AspNetRoles] ([Id])
+ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetRoleClaims] CHECK CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId]
+
+CREATE TABLE [dbo].[AspNetUserClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+ALTER TABLE [dbo].[AspNetUserClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserClaims] CHECK CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId]
+
+CREATE TABLE [dbo].[AspNetUserLogins](
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[ProviderKey] [nvarchar](128) NOT NULL,
+	[ProviderDisplayName] [nvarchar](max) NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+ CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY CLUSTERED 
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+ALTER TABLE [dbo].[AspNetUserLogins]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserLogins] CHECK CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId]
+
+CREATE TABLE [dbo].[AspNetUserRoles](
+	[UserId] [nvarchar](450) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+ CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AspNetRoles] ([Id])
+ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId]
+
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId]
+
+CREATE TABLE [dbo].[AspNetUserTokens](
+	[UserId] [nvarchar](450) NOT NULL,
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](128) NOT NULL,
+	[Value] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[LoginProvider] ASC,
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+ALTER TABLE [dbo].[AspNetUserTokens]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+
+ALTER TABLE [dbo].[AspNetUserTokens] CHECK CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId]
+
 CREATE TABLE [plataforma_iot]..[Usuario](
 	[id]			INT IDENTITY(1,1)	PRIMARY KEY,
 	[email]			VARCHAR(48)			UNIQUE	NOT NULL,
@@ -59,124 +205,126 @@ CREATE TABLE [plataforma_iot]..[Datos](
 	CONSTRAINT [fk_datos_sensor]	FOREIGN KEY ([fk_idsensor]) REFERENCES [plataforma_iot]..[Sensor]([id]) ON DELETE CASCADE
 );
 
-USE plataforma_iot
-
-GO
 
 -- INSERTAR DATOS EN LA BASE DE DATOS
----- usuarios
-SET IDENTITY_INSERT [plataforma_iot]..[Usuario] ON;
+INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId],[ProductVersion])
+VALUES ('00000000000000_CreateIdentitySchema','3.1.4')
 
-INSERT INTO [plataforma_iot]..[Usuario]
-           ([id], [email], [contrasenya], [nombre], [apellidos])
-     VALUES (0,'kensington@mail.com',CAST('contrasenya' AS VARBINARY(128)),'ken','sington'),
-			(1, 'roccat@mail.com', CAST('contrasenya' AS VARBINARY(128)),'roccat','kanga');
+-- DATOS PARA PRUEBAS
+IF(@DatosDePrueba = 1)
+BEGIN
+	---- usuarios
+	SET IDENTITY_INSERT [plataforma_iot]..[Usuario] ON;
 
-SET IDENTITY_INSERT [plataforma_iot]..[Usuario] OFF;
+	INSERT INTO [plataforma_iot]..[Usuario]
+			   ([id], [email], [contrasenya], [nombre], [apellidos])
+		 VALUES (0,'kensington@mail.com',CAST('contrasenya' AS VARBINARY(128)),'ken','sington'),
+				(1, 'roccat@mail.com', CAST('contrasenya' AS VARBINARY(128)),'roccat','kanga');
 
----- proyectos
-SET IDENTITY_INSERT [plataforma_iot]..[Proyecto] ON;
+	SET IDENTITY_INSERT [plataforma_iot]..[Usuario] OFF;
 
-INSERT INTO [plataforma_iot]..[Proyecto]
-			([id],[nombre],[descripcion])
-	VALUES	(0,'Escarlet', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-			(1,'Neo','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-			(2,'Escorpio','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-			(3,'Lockheart','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+	---- proyectos
+	SET IDENTITY_INSERT [plataforma_iot]..[Proyecto] ON;
 
-SET IDENTITY_INSERT [plataforma_iot]..[Proyecto] OFF;
+	INSERT INTO [plataforma_iot]..[Proyecto]
+				([id],[nombre],[descripcion])
+		VALUES	(0,'Escarlet', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+				(1,'Neo','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+				(2,'Escorpio','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+				(3,'Lockheart','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
----- usuario en proyecto
+	SET IDENTITY_INSERT [plataforma_iot]..[Proyecto] OFF;
+
+	---- usuario en proyecto
+	INSERT INTO [plataforma_iot]..[Usuario_en_Proyecto]
+			   ([id_usuario],[id_proyecto])
+		 VALUES	(0,1),
+				(1,0),
+				(1,2),
+				(1,3);
 
 
-INSERT INTO [plataforma_iot]..[Usuario_en_Proyecto]
-           ([id_usuario],[id_proyecto])
-     VALUES	(0,1),
-			(1,0),
-			(1,2),
-			(1,3);
 
+	---- estaciones base
+	SET IDENTITY_INSERT [plataforma_iot]..[EstacionBase] ON;
 
+	INSERT INTO [plataforma_iot]..[EstacionBase]
+				([id],[nombre],[fk_idproyecto])
+			VALUES	(0,'EB00',0),
+					(1,'EB01',0),
+					(2,'EB02',1),
+					(3,'EB03',2);
 
----- estaciones base
-SET IDENTITY_INSERT [plataforma_iot]..[EstacionBase] ON;
+	SET IDENTITY_INSERT [plataforma_iot]..[EstacionBase] OFF;
 
-INSERT INTO [plataforma_iot]..[EstacionBase]
-			([id],[nombre],[fk_idproyecto])
-		VALUES	(0,'EB00',0),
-				(1,'EB01',0),
-				(2,'EB02',1),
-				(3,'EB03',2);
+	---- sensores
+	SET IDENTITY_INSERT [plataforma_iot]..[Sensor] ON;
 
-SET IDENTITY_INSERT [plataforma_iot]..[EstacionBase] OFF;
+	INSERT INTO [plataforma_iot]..[Sensor]
+			   ([id],[nombre],[longitud],[latitud],[fk_idestacionbase])
+		 VALUES
+			   (0,'SE00','-0,780410','38,193270',0),
+			   (1,'SE01','-0,782856','38,190015',0),
+			   (6,'SE02','-0,743588','38,169475',0),
+			   (7,'SE03','-0,751807','38,166119',0),
+			   (8,'SE04','-0,717310','38,173168',0),
+			   (2,'SE10','-0,528031','38,2781293',1),
+			   (3,'SE11','-0,530436','38,280800',1),
+			   (4,'SE12','-0,530436','38,280800',2),
+			   (5,'SE03','-0,535455','38,275500',3);
 
----- sensores
-SET IDENTITY_INSERT [plataforma_iot]..[Sensor] ON;
+	SET IDENTITY_INSERT [plataforma_iot]..[Sensor] OFF;
 
-INSERT INTO [plataforma_iot]..[Sensor]
-           ([id],[nombre],[longitud],[latitud],[fk_idestacionbase])
-     VALUES
-           (0,'SE00','-0,780410','38,193270',0),
-		   (1,'SE01','-0,782856','38,190015',0),
-		   (6,'SE02','-0,743588','38,169475',0),
-		   (7,'SE03','-0,751807','38,166119',0),
-		   (8,'SE04','-0,717310','38,173168',0),
-		   (2,'SE10','-0,528031','38,2781293',1),
-		   (3,'SE11','-0,530436','38,280800',1),
-		   (4,'SE12','-0,530436','38,280800',2),
-		   (5,'SE03','-0,535455','38,275500',3);
+	---- datos
 
-SET IDENTITY_INSERT [plataforma_iot]..[Sensor] OFF;
+	INSERT INTO [plataforma_iot]..[Datos]
+			   ([stamp],[fk_idsensor],[humedad],[temperatura])
+		 VALUES
+			   ('2020-03-31 11:55:00',0,40.75,20.05),
+			   ('2020-03-31 12:00:00',0,41.75,20.05),
+			   ('2020-03-31 12:05:00',0,40.01,20.05),
+			   ('2020-03-31 12:10:00',0,40.5,20.05),
 
----- datos
+			   ('2020-03-31 11:55:00',1,35.7,25.53),
+			   ('2020-03-31 12:00:00',1,35.8,25.55),
+			   ('2020-03-31 12:05:00',1,35.95,25.53),
 
-INSERT INTO [plataforma_iot]..[Datos]
-           ([stamp],[fk_idsensor],[humedad],[temperatura])
-     VALUES
-           ('2020-03-31 11:55:00',0,40.75,20.05),
-		   ('2020-03-31 12:00:00',0,41.75,20.05),
-		   ('2020-03-31 12:05:00',0,40.01,20.05),
-		   ('2020-03-31 12:10:00',0,40.5,20.05),
+			   ('2020-03-31 11:55:00',2,35.7,25.53),
+			   ('2020-03-31 12:00:00',2,35.8,25.55),
+			   ('2020-03-31 12:05:00',2,35.95,25.54),
+			   ('2020-03-31 12:10:00',2,35.89,25.55),
+			   ('2020-03-31 12:15:00',2,35.75,25.56),
+			   ('2020-03-31 12:20:00',2,35.8,25.53),
 
-		   ('2020-03-31 11:55:00',1,35.7,25.53),
-		   ('2020-03-31 12:00:00',1,35.8,25.55),
-		   ('2020-03-31 12:05:00',1,35.95,25.53),
+			   ('2020-03-31 11:55:00',3,35.7,25.53),
+			   ('2020-03-31 12:00:00',3,35.8,25.55),
+			   ('2020-03-31 12:05:00',3,35.95,25.54),
 
-		   ('2020-03-31 11:55:00',2,35.7,25.53),
-		   ('2020-03-31 12:00:00',2,35.8,25.55),
-		   ('2020-03-31 12:05:00',2,35.95,25.54),
-		   ('2020-03-31 12:10:00',2,35.89,25.55),
-		   ('2020-03-31 12:15:00',2,35.75,25.56),
-		   ('2020-03-31 12:20:00',2,35.8,25.53),
+			   ('2020-03-31 11:55:00',4,35.7,25.53),
+			   ('2020-03-31 12:00:00',4,30.8,20.55),
+			   ('2020-03-31 12:05:00',4,32.95,29.54),
 
-		   ('2020-03-31 11:55:00',3,35.7,25.53),
-		   ('2020-03-31 12:00:00',3,35.8,25.55),
-		   ('2020-03-31 12:05:00',3,35.95,25.54),
+			   ('2020-03-31 11:55:00',5,35.7,25.53),
+			   ('2020-03-31 12:00:00',5,35.8,25.55),
+			   ('2020-03-31 12:05:00',5,35.95,25.54),
+			   ('2020-03-31 12:10:00',5,35.89,25.55),
+			   ('2020-03-31 12:15:00',5,35.75,25.56),
 
-		   ('2020-03-31 11:55:00',4,35.7,25.53),
-		   ('2020-03-31 12:00:00',4,30.8,20.55),
-		   ('2020-03-31 12:05:00',4,32.95,29.54),
+			   ('2020-03-31 11:50:00',6,35.8,26.0245),
+			   ('2020-03-31 11:55:00',6,35.7,25.53),
+			   ('2020-03-31 12:00:00',6,35.8,24.55),
+			   ('2020-03-31 12:05:00',6,35.95,25.54),
+			   ('2020-03-31 12:10:00',6,32.89,28.55),
+			   ('2020-03-31 12:15:00',6,33.75,29.56),
+			   ('2020-03-31 12:20:00',6,37.8,30.53),
 
-		   ('2020-03-31 11:55:00',5,35.7,25.53),
-		   ('2020-03-31 12:00:00',5,35.8,25.55),
-		   ('2020-03-31 12:05:00',5,35.95,25.54),
-		   ('2020-03-31 12:10:00',5,35.89,25.55),
-		   ('2020-03-31 12:15:00',5,35.75,25.56),
+			   ('2020-03-31 11:55:00',7,35.7,20.753),
+			   ('2020-03-31 12:00:00',7,34.8,24.579),
+			   ('2020-03-31 12:05:00',7,35.95,22.169),
 
-		   ('2020-03-31 11:50:00',6,35.8,26.0245),
-		   ('2020-03-31 11:55:00',6,35.7,25.53),
-		   ('2020-03-31 12:00:00',6,35.8,24.55),
-		   ('2020-03-31 12:05:00',6,35.95,25.54),
-		   ('2020-03-31 12:10:00',6,32.89,28.55),
-		   ('2020-03-31 12:15:00',6,33.75,29.56),
-		   ('2020-03-31 12:20:00',6,37.8,30.53),
-
-		   ('2020-03-31 11:55:00',7,35.7,20.753),
-		   ('2020-03-31 12:00:00',7,34.8,24.579),
-		   ('2020-03-31 12:05:00',7,35.95,22.169),
-
-		   ('2020-03-31 12:10:00',8,35.89,20.55),
-		   ('2020-03-31 12:15:00',8,34.983,21.56),
-		   ('2020-03-31 12:20:00',8,35.218,22.53);
+			   ('2020-03-31 12:10:00',8,35.89,20.55),
+			   ('2020-03-31 12:15:00',8,34.983,21.56),
+			   ('2020-03-31 12:20:00',8,35.218,22.53);
+END
 
 EXEC sp_MSforeachtable 'SELECT * FROM ?';
